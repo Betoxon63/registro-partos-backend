090143e18622a82968df6e9d6aa30511407c3059
Betoxon63 <betoxon63@gmail.com>
2025-09-28 03:01:43 -0300

    Commit inicial de Spring Initializr con dependencias web, JPA, seguridad y PostgreSQL.

---
+++ src/main/java/cl/hospital/registro_partos/RegistroPartosApplication.java
@@ -0,0 +1,13 @@
+package cl.hospital.registro_partos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroPartosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroPartosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/hospital/registro_partos/RegistroPartosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.hospital.registro_partos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroPartosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
